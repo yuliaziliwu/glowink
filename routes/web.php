29cc7d00d2e43ca8758ae422b8cb62b491995d3b
fabianggara <159712456+fabianggara@users.noreply.github.com>
2024-03-06 15:36:01 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,10 @@ use App\Http\Controllers\logincontroller;
 use App\Http\Controllers\registercontroller;
 
 
-<<<<<<< HEAD
 Route::get('/', function () {
     return view('dashboard');
-=======
-
-Route::get('/', function() { 
-    return view('welcome');
->>>>>>> ce273e6d675857d0547d69aa9df3f1d750b08794
 });
+
 
 Route::get('/view/login', function () {
     return view('login');
@@ -21,3 +16,4 @@ Route::get('/view/login', function () {
 
 Route::get('/login', [logincontroller::class, 'login']);
 Route::get('/register', [registercontroller::class, 'register']);
+
