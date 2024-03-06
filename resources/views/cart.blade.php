136c96aeb37b343555f65fb79b124f477595a64d
shandymaiza <shandymaiza04@gmail.com>
2024-03-06 17:48:32 +0700

    view dan controller cart

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,4 @@
+
+<div>
+    <h1>Ini Halaman keranjang kelompok 5 IF2B</h2>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\logincontroller;
 use App\Http\Controllers\registercontroller;
 use App\Http\Controllers\checkoutcontroller;
+use App\Http\Controllers\cartcontroller;
 
 Route::get('/', function () {
     return view('dashboard');
@@ -16,5 +17,6 @@ Route::get('/view/login', function () {
 Route::get('/login', [logincontroller::class, 'login']);
 Route::get('/register', [registercontroller::class, 'register']);
 Route::get('/checkout', [checkoutcontroller::class, 'checkout']);
+Route::get('/cart', [cartcontroller::class, 'cart']);
 
 
