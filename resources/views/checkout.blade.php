9223ee197e63eeddb5a07311078e788b3a046d8f
Yulia Pipka <yuliaziliwu59@sma.belajar.id>
2024-03-06 16:25:38 +0700

    buat bagian checkout view dan controller, route juga

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Ini Halaman Checkout Kelompok 5 IF2B Pagi</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\logincontroller;
 use App\Http\Controllers\registercontroller;
-
+use App\Http\Controllers\checkoutcontroller;
 
 Route::get('/', function () {
     return view('dashboard');
@@ -16,4 +15,6 @@ Route::get('/view/login', function () {
 
 Route::get('/login', [logincontroller::class, 'login']);
 Route::get('/register', [registercontroller::class, 'register']);
+Route::get('/checkout', [checkoutcontroller::class, 'checkout']);
+
 
