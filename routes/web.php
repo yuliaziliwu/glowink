16f22cad1b0e6e3db5da30d5c693877440f93fb7
shandymaiza <shandymaiza04@gmail.com>
2024-03-08 09:02:37 +0700

    halaman dashboar

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\logincontroller;
 use App\Http\Controllers\registercontroller;
 use App\Http\Controllers\checkoutcontroller;
 use App\Http\Controllers\cartcontroller;
+use App\Http\Controllers\halamancontroller;
 
 Route::get('/', function () {
     return view('dashboard');
@@ -14,9 +15,14 @@ Route::get('/view/login', function () {
     return view('login');
 });
 
+Route::get('/view/halaman', function () {
+    return view('halaman');
+});
+
 Route::get('/login', [logincontroller::class, 'login']);
 Route::get('/register', [registercontroller::class, 'register']);
 Route::get('/checkout', [checkoutcontroller::class, 'checkout']);
 Route::get('/cart', [cartcontroller::class, 'cart']);
+Route::get('/halaman', [halamancontroller::class, 'halaman']);
 
 
