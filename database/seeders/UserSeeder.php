7004a44ab76c74ecf4764052a83b827761628397
RikardoAnju <160112729+RikardoAnju@users.noreply.github.com>
2024-03-24 14:47:42 +0700

    pembuatan token Api dan Pembuatan Api Kategori

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'email_verified_at' => now(),  
+        ]);
+    }
+}
